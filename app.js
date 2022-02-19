c3a86b70bab0973165ad24b1042e77997b0240ae
Cedric Le Sergent <lesergent.cedric@gmail.com>
2022-02-19 06:27:13 +0100

    Add Accordeon Active Class

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const askTitle = document.getElementsByClassName('label')
 for(i=0; i<accordeon.length; i++) {
     accordeon[i].addEventListener('click', function() {
         console.log(accordeon);
-        this.classList.toggle('active')
+        this.classList.toggle('active');
     })
 }
-console.log("hello World");
